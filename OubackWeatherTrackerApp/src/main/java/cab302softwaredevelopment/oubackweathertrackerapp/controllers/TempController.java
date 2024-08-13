a5572eb1f308094270e24ebebe8785f33a9c20ec
Jimbo-Wondall <109582514+Jimbo-Wondall@users.noreply.github.com>
2024-08-13 13:37:22 +1000

    Added test components

--- OubackWeatherTrackerApp/src/main/java/cab302softwaredevelopment/oubackweathertrackerapp/controllers/TempController.java
+++ OubackWeatherTrackerApp/src/main/java/cab302softwaredevelopment/oubackweathertrackerapp/controllers/TempController.java
@@ -1,14 +1,24 @@
 package cab302softwaredevelopment.oubackweathertrackerapp.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class TempController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button cancelButton;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onCancelButtonClick() {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
     }
 }
