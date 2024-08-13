0c541071dda40caf5ffb2b301f5c5bd879d61959
SegusFaultise <wilsonzakk8@gmail.com>
2024-08-13 12:45:18 +1000

    misc

--- demo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module cab302softwaredevelopment.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens cab302softwaredevelopment.demo to javafx.fxml;
-    exports cab302softwaredevelopment.demo;
-}
