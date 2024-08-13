431f557929eee4cb975421212d152a169a5c7ba6
SegusFaultise <wilsonzakk8@gmail.com>
2024-08-13 12:35:45 +1000

    FIlestructure change

--- OutbackWeatherTrackerApp/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.cab302 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.cab302 to javafx.fxml;
-    exports com.example.cab302;
-}
