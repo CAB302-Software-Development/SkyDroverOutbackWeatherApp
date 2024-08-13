b5acd05ed2663cb0b96d429550cea3e143f3771c
SegusFaultise <wilsonzakk8@gmail.com>
2024-08-13 13:17:03 +1000

    misc

--- OubackWeatherTrackerApp/src/main/java/cab302softwaredevelopment/oubackweathertrackerapp/ApplicationEntry.java
+++ OubackWeatherTrackerApp/src/main/java/cab302softwaredevelopment/oubackweathertrackerapp/ApplicationEntry.java
@@ -4,16 +4,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import org.junit.Test;
+
 import java.io.IOException;
 
-public class EntryApplication extends javafx.application.Application {
+public class ApplicationEntry extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(EntryApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ApplicationEntry.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+    }
+
+    @Test
+    public void test() {
     }
 
     public static void main(String[] args) {
--- OubackWeatherTrackerApp/src/main/java/module-info.java
+++ OubackWeatherTrackerApp/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module cab302softwaredevelopment.oubackweathertrackerapp {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires junit;
 
     opens cab302softwaredevelopment.oubackweathertrackerapp to javafx.fxml;
     exports cab302softwaredevelopment.oubackweathertrackerapp;
