9c52677e3d480d554c4bb67a429635d8dbe9982a
SegusFaultise <wilsonzakk8@gmail.com>
2024-08-19 13:06:36 +1000

    ADDED: .env file

--- OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/restapis/NasaApi.java
+++ OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/restapis/NasaApi.java
@@ -1,4 +1,9 @@
 package cab302softwaredevelopment.outbackweathertrackerapplication.restapis;
 
 public class NasaApi {
+  String apiKey = System.getenv("NASA_API_KEY");
+
+  public void SetupApi() throws IllegalAccessException {
+    System.out.println(apiKey);
+  }
 }
