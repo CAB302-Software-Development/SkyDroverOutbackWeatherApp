f6c2899fc4f2cb53e2d88d9367340ea1efedeab8
Jimbo-Wondall <109582514+Jimbo-Wondall@users.noreply.github.com>
2024-09-06 14:43:34 +1000

    Added opens/exports for Gson

--- OutbackWeatherTrackerApplication/src/main/java/module-info.java
+++ OutbackWeatherTrackerApplication/src/main/java/module-info.java
@@ -4,11 +4,21 @@ module cab302softwaredevelopment.outbackweathertrackerapplication {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
-  requires jdk.jdi;
-  requires java.sql;
+    requires jdk.jdi;
+    requires java.sql;
+    requires com.google.gson;
 
-  opens cab302softwaredevelopment.outbackweathertrackerapplication to javafx.fxml;
     exports cab302softwaredevelopment.outbackweathertrackerapplication;
+    exports cab302softwaredevelopment.outbackweathertrackerapplication.models;
     exports cab302softwaredevelopment.outbackweathertrackerapplication.controllers;
-    opens cab302softwaredevelopment.outbackweathertrackerapplication.controllers to javafx.fxml;
+    exports cab302softwaredevelopment.outbackweathertrackerapplication.controllers.pages;
+
+    exports cab302softwaredevelopment.outbackweathertrackerapplication.services to com.google.gson;
+    exports cab302softwaredevelopment.outbackweathertrackerapplication.controllers.widgets to javafx.fxml;
+
+    opens cab302softwaredevelopment.outbackweathertrackerapplication to javafx.fxml;
+    opens cab302softwaredevelopment.outbackweathertrackerapplication.controllers to com.google.gson, javafx.fxml;
+    opens cab302softwaredevelopment.outbackweathertrackerapplication.controllers.pages to com.google.gson, javafx.fxml;
+    opens cab302softwaredevelopment.outbackweathertrackerapplication.models to com.google.gson, javafx.fxml;
+    opens cab302softwaredevelopment.outbackweathertrackerapplication.services to com.google.gson;
 }
