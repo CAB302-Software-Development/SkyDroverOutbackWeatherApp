d7676888c06669c1ee2ea2d75656afdd6767006a
SegusFaultise <wilsonzakk8@gmail.com>
2024-09-11 13:36:39 +1000

    Deleted restapi dir, it was not needed due to the restapi being in another repo.

--- OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/restapis/NasaApi.java
+++
@@ -1,9 +0,0 @@
-package cab302softwaredevelopment.outbackweathertrackerapplication.restapis;
-
-public class NasaApi {
-  String apiKey = System.getenv("NASA_API_KEY");
-
-  public void SetupApi() throws IllegalAccessException {
-    System.out.println(apiKey);
-  }
-}
