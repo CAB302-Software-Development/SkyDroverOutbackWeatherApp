e388e8184a821d4455de070e726d7053ff49dbe3
Jimbo-Wondall <109582514+Jimbo-Wondall@users.noreply.github.com>
2024-09-17 02:28:46 +1000

    Removed unused imports

--- OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/controllers/pages/MapController.java
+++ OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/controllers/pages/MapController.java
@@ -1,10 +1,5 @@
 package cab302softwaredevelopment.outbackweathertrackerapplication.controllers.pages;
 
-import cab302softwaredevelopment.outbackweathertrackerapplication.models.ISwapPanel;
-import javafx.fxml.FXML;
-import javafx.scene.layout.Pane;
-import org.kordamp.bootstrapfx.scene.layout.Panel;
-
 public class MapController {
 
 }
