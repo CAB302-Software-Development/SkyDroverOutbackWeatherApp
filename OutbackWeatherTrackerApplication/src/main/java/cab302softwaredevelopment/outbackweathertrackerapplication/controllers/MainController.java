52493eb507c45a9be5eb432b7c837b80c86bb11b
Jimbo-Wondall <109582514+Jimbo-Wondall@users.noreply.github.com>
2024-09-16 20:12:56 +1000

    Cleaned up unused code

--- OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/controllers/MainController.java
+++ OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/controllers/MainController.java
@@ -1,29 +1,19 @@
 package cab302softwaredevelopment.outbackweathertrackerapplication.controllers;
 
 import cab302softwaredevelopment.outbackweathertrackerapplication.ApplicationEntry;
-import cab302softwaredevelopment.outbackweathertrackerapplication.models.ISwapPanel;
 import cab302softwaredevelopment.outbackweathertrackerapplication.services.PreferencesService;
-import cab302softwaredevelopment.outbackweathertrackerapplication.utils.Logger;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.SVGPath;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.IOException;
 import java.net.URL;
-import java.util.*;
+import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
     public static final int WIDTH = 1080;
@@ -33,8 +23,6 @@ public class MainController implements Initializable {
 
     @FXML
     public Button btnProfile, btnDashboard, btnMap, btnForecast, btnSettings, btnDrawer, btnAlerts, btnReports;
-    @FXML
-    SVGPath icoProfile, icoDashboard, icoMap, icoForecast, icoSettings, icoDrawer, icoAlerts, icoReports;
     @FXML
     public VBox vbNavbar;
     private Scene scene;
@@ -53,8 +41,6 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        loadSVGs();
-
         controller = this;
         Node swpProfile = createSwapPanel("panels/profile-panel.fxml", btnProfile);
         Node swpDashboard = createSwapPanel("panels/dashboard-panel.fxml", btnDashboard);
@@ -62,21 +48,6 @@ public class MainController implements Initializable {
         Node swpMap = createSwapPanel("panels/map-panel.fxml", btnMap);
         Node swpSettings = createSwapPanel("panels/settings-panel.fxml", btnSettings);
         root.centerProperty().set(swpDashboard);
-    }
-
-    private void loadSVGs() {
-        // Set<Node> svgs = root.lookupAll(".icon");
-        // Node temp = ((Button)svgs.iterator().next()).getGraphic();
-        // svgs.forEach(s -> setSvgPathFromFile((SVGPath) s, s.parentProperty().get()));
-
-        //setSvgPathFromFile(icoProfile);
-        //setSvgPathFromFile(icoDashboard);
-        //setSvgPathFromFile(icoForecast);
-        //setSvgPathFromFile(icoMap);
-        //setSvgPathFromFile(icoSettings, btnSettings);
-        //setSvgPathFromFile(icoDrawer);
-        //setSvgPathFromFile(icoAlerts);
-        //setSvgPathFromFile(icoReports);
     }
 
     private Node createSwapPanel(String fxmlPath, Button button) {
@@ -88,33 +59,6 @@ public class MainController implements Initializable {
             throw new RuntimeException(e);
         }
         button.setOnAction(actionEvent -> root.centerProperty().set(panelNode));
-        //button.getGraphic().scaleXProperty().bind(button.widthProperty().divide(1000));
-        //button.getGraphic().scaleYProperty().bind(button.heightProperty().divide(1000));
         return panelNode;
-    }
-
-    /**
-     * Sets the SVG content of an SVGPath element from a file.
-     *
-     * @param svgPath the SVGPath node to update
-     */
-    private void setSvgPathFromFile(SVGPath svgPath, Parent parent) {
-        try {
-            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            Document document = builder.parse((String) svgPath.getUserData());
-
-            NodeList pathElements = document.getElementsByTagName("path");
-            if (pathElements.getLength() > 0) {
-                String pathData = pathElements.item(0).getAttributes().getNamedItem("d").getNodeValue();
-                svgPath.setContent(pathData);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        // Node node = svgPath.getParent();
-        // Button container = (Button) node;
-        //Scene container = parent.getScene();
-        //svgPath.scaleXProperty().bind(container.widthProperty().divide(1000));
-        //svgPath.scaleYProperty().bind(container.heightProperty().divide(1000));
     }
 }
