b73b61b1b0c8060a60797af8f6d48b3e657b3992
Jimbo-Wondall <109582514+Jimbo-Wondall@users.noreply.github.com>
2024-09-17 10:49:15 +1000

    Updated to use null check rather than try... catch

--- OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/controllers/windows/LoginController.java
+++ OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/controllers/windows/LoginController.java
@@ -4,8 +4,6 @@ import cab302softwaredevelopment.outbackweathertrackerapplication.ApplicationEnt
 import cab302softwaredevelopment.outbackweathertrackerapplication.database.dao.AccountDAO;
 import cab302softwaredevelopment.outbackweathertrackerapplication.database.model.Account;
 import cab302softwaredevelopment.outbackweathertrackerapplication.services.LoginState;
-import cab302softwaredevelopment.outbackweathertrackerapplication.utils.Logger;
-import jakarta.persistence.NoResultException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,6 +13,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import javafx.event.ActionEvent;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
@@ -101,111 +100,78 @@ public class LoginController {
         isLogin = true;
     }
 
-    /**
-     * Action handler for the "Login" button
-      */
     @FXML
-    private void handleLogin() {
-        if(!isLogin) return;
+    private void handleButtonPress(ActionEvent e) {
+        if (e.getSource() == loginButton) {
+            String email = emailTextFieldLogin.getText();
+            String password = passwordTextFieldLogin.getText();
+            if (handleLogin(email, password)) {
+                continueToApplication();
+            }
+        } else if (e.getSource() == signupButton) {
+            String email = emailTextFieldSignup.getText();
+            String password = passwordTextFieldSignup.getText();
+            if (handleSignUp(email, password)) {
+                continueToApplication();
+            }
+        } else if (e.getSource() == guestLinkLogin || e.getSource() == guestLinkSignup) {
+            continueToApplication();
+        }
+    }
 
-        String email = emailTextFieldLogin.getText();
-        String password = passwordTextFieldLogin.getText();
-
+    public boolean handleLogin(String email, String password) {
         if (isInvalidEmail(email)) {
             showErrorMessage("Invalid email format.");
-            return;
+            return false;
         }
 
         if (isInvalidPassword(password)) {
-            showErrorMessage("Password must be at least 8 characters, include one uppercase letter, one digit, and one special character.");
-            return;
+            showErrorMessage("Password must be at least 8 characters, " +
+                    "include one uppercase letter, one digit, and one special character.");
+            return false;
         }
 
         AccountDAO accountDAO = new AccountDAO();
-        Account account;
-        try {
-            account = accountDAO.getByEmail(email);
-        } catch (NoResultException e) {
+        Account account = accountDAO.getByEmail(email);
+        if (account == null) {
             showErrorMessage("No account exists with specified email.");
-            return;
+            return false;
         }
 
         if (!account.verifyPassword(password)) {
             showErrorMessage("Incorrect password");
-            return;
+            return false;
         }
 
         LoginState.login(account);
-
-        try {
-            continueToApplication();
-        } catch (IOException e) {
-            showErrorMessage("Error starting application");
-            e.printStackTrace();
-        }
+        return true;
     }
 
-    /**
-     * Action handler for the "Sign Up" button
-     */
-    @FXML
-    private void handleSignUp() {
-        if(isLogin) return;
-
-        String email = emailTextFieldSignup.getText();
-        String password = passwordTextFieldSignup.getText();
-
+    public boolean handleSignUp(String email, String password) {
         if (isInvalidEmail(email)) {
             showErrorMessage("Invalid email format.");
-            return;
+            return false;
         }
 
         if (isInvalidPassword(password)) {
             showErrorMessage("Password must be at least 8 characters, include one uppercase letter, one digit, and one special character.");
-            return;
+            return false;
         }
 
         AccountDAO accountDAO = new AccountDAO();
 
-        try {
-            if (accountDAO.getByEmail(email) != null) {
-                showErrorMessage("User already exists with this email.");
-                return;
-            }
-        } catch (NoResultException e) {
-            Logger.printLog("User does not already exist. Creating account...");
+        if (accountDAO.getByEmail(email) != null) {
+            showErrorMessage("User already exists with this email.");
+            return false;
         }
 
         Account account = new Account(email, password, true);
 
-        try {
-            accountDAO.insert(account);
-            accountDAO.getByEmail(email);
-        } catch (Exception e) {
-            showErrorMessage("Error creating new account.");
-            e.printStackTrace();
-            return;
-        }
+        accountDAO.insert(account);
 
-        Logger.printLog("Account created with email: " + email);
         LoginState.login(account);
 
-        try {
-            continueToApplication();
-        } catch (IOException e) {
-            showErrorMessage("Error starting application");
-            e.printStackTrace();
-        }
-    }
-
-    @FXML
-    private void handleGuestAccess() {
-        try {
-            continueToApplication();
-        } catch (IOException e) {
-            showErrorMessage("Error starting application");
-            e.printStackTrace();
-        }
+        return true;
     }
 
     /**
@@ -244,7 +210,6 @@ public class LoginController {
             signupMessageLabel.setVisible(true);
             loginMessageLabel.setVisible(false);
         }
-        clearInputFields();
     }
 
     private void clearInputFields() {
@@ -256,21 +221,25 @@ public class LoginController {
 
     /**
      * Closes the login / sign up window and initializes the main application view
-     * @throws IOException If FXML load fails
      */
-    private void continueToApplication() throws IOException {
-        ((Stage) loginPane.getScene().getWindow()).close();
+    private void continueToApplication() {
+        try {
+            ((Stage) loginPane.getScene().getWindow()).close();
 
-        FXMLLoader loader = new FXMLLoader(ApplicationEntry.class.getResource("windows/main-view.fxml"));
-        Parent root = loader.load();
-        MainController controller = loader.getController();
+            FXMLLoader loader = new FXMLLoader(ApplicationEntry.class.getResource("windows/main-view.fxml"));
+            Parent root = loader.load();
+            MainController controller = loader.getController();
 
-        Scene scene = new Scene(root, MainController.WIDTH, MainController.HEIGHT);
-        controller.setScene(scene);
+            Scene scene = new Scene(root, MainController.WIDTH, MainController.HEIGHT);
+            controller.setScene(scene);
 
-        Stage mainStage = new Stage();
-        mainStage.setTitle(ApplicationEntry.stageTitle);
-        mainStage.setScene(scene);
-        mainStage.show();
+            Stage mainStage = new Stage();
+            mainStage.setTitle(ApplicationEntry.stageTitle);
+            mainStage.setScene(scene);
+            mainStage.show();
+        } catch (IOException e) {
+            showErrorMessage("Error starting application");
+            e.printStackTrace();
+        }
     }
 }
