ac1bf86be47d200fd848fb71fb99f5ef5f9c8651
SegusFaultise <wilsonzakk8@gmail.com>
2024-09-17 14:50:31 +1000

    Added javafx-web dependency to pom file, add map/ dir this contains map.html, styles.css and script.js these build the map and display the map, I've also updated the MapController.java to render the map when navigating to the map-panel.fxml

--- OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/controllers/pages/MapController.java
+++ OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/controllers/pages/MapController.java
@@ -1,5 +1,24 @@
 package cab302softwaredevelopment.outbackweathertrackerapplication.controllers.pages;
 
+import javafx.fxml.FXML;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+import java.io.File;
+
+
 public class MapController {
 
+  @FXML
+  private WebView mapWebView;
+
+  @FXML
+  public void initialize() {
+    WebEngine webEngine = mapWebView.getEngine();
+
+    String htmlFile = getClass().getResource(
+            "/cab302softwaredevelopment/outbackweathertrackerapplication/map/map.html")
+        .toExternalForm();
+    webEngine.load(htmlFile);
+  }
 }
---
+++ OutbackWeatherTrackerApplication/src/main/resources/cab302softwaredevelopment/outbackweathertrackerapplication/map/script.js
@@ -0,0 +1,11 @@
+var mapOptions = {
+  center: [17.385044, 78.486671],
+  zoom: 10
+}
+
+var map = new L.map('map', mapOptions);
+
+var layer = new L.TileLayer(
+    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+map.addLayer(layer);
