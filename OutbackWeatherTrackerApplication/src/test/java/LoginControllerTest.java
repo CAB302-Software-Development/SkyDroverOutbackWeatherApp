2430b16abbfe58e46f1bc2fadc97413f1745f0a8
Jimbo-Wondall <109582514+Jimbo-Wondall@users.noreply.github.com>
2024-09-17 12:33:22 +1000

    Added placeholder tests for Login, Preferences, and Settings

---
+++ OutbackWeatherTrackerApplication/src/test/java/LoginControllerTest.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import cab302softwaredevelopment.outbackweathertrackerapplication.controllers.windows.LoginController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+public class LoginControllerTest extends ApplicationTest  {
+    private LoginController loginController;
+
+    @BeforeEach
+    public void setUp() {
+        loginController = new LoginController();
+    }
+
+    @Test
+    public void testLoginSuccess() {
+        boolean result = loginController.handleLogin("test1@gmail.com", "SecurePass1!");
+        assertTrue(result, "Login should succeed with valid credentials.");
+    }
+
+    @Test
+    public void testLoginFailure1() {
+        boolean result = loginController.handleLogin("12224g", "aaaaaaa");
+        assertFalse(result, "Login should fail with invalid credentials.");
+    }
+
+    @Test
+    public void testSignupFailure1() {
+        boolean result = loginController.handleSignUp("email@@example.com", "SecurePass1!");
+        assertFalse(result, "Login should fail with invalid credentials.");
+    }
+
+    @Test
+    public void testSignupFailure2() {
+        boolean result = loginController.handleSignUp("email@example.com", "insecurepass1");
+        assertFalse(result, "Login should fail with invalid credentials.");
+    }
+}
---
+++ OutbackWeatherTrackerApplication/src/test/java/SettingsControllerTest.java
@@ -0,0 +1,12 @@
+import cab302softwaredevelopment.outbackweathertrackerapplication.controllers.pages.SettingsController;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SettingsControllerTest {
+
+    private SettingsController settingsController;
+
+    @BeforeEach
+    public void setUp() {
+        settingsController = new SettingsController();
+    }
+}
