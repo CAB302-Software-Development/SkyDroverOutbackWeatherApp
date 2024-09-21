facb5bcdd3db1094c35ed6808f8616b1e83ed444
joshua ward <joshuaward1000@gmail.com>
2024-09-22 01:37:42 +1000

    Added documentation for Locations

--- OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/database/model/Location.java
+++ OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/database/model/Location.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.OnDelete;
@@ -15,31 +17,50 @@ import org.hibernate.annotations.OnDeleteAction;
 
 @Entity(name = "location")
 @Table(name = "location")
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class Location {
 
+  /**
+   * The ID of the location.
+   */
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Getter @Setter
   private Integer id;
 
+  /**
+   * The account that this location is associated with.
+   */
   @ManyToOne
   @PrimaryKeyJoinColumn
   @OnDelete(action = OnDeleteAction.CASCADE)
   @Getter
   private Account account;
 
+  /**
+   * The longitude of the location.
+   */
   @Column(name="longitude", nullable = false)
   @Getter
   private Double longitude;
 
+  /**
+   * The latitude of the location.
+   */
   @Column(name="latitude", nullable = false)
   @Getter
   private Double latitude;
 
+  /**
+   * The elevation of the location.
+   */
   @Column(name="elevation", nullable = false)
   @Getter
   private Double elevation;
 
+  /**
+   * The name of the location.
+   */
   @Column(name="name", nullable = false)
   @Getter
   private String name;
@@ -47,15 +68,14 @@ public class Location {
   public Location() {
   }
 
-  public Location(Integer id,Account account, Double longitude, Double latitude, Double elevation, String name) {
-    this.id = id;
-    this.account = account;
-    this.longitude = longitude;
-    this.latitude = latitude;
-    this.elevation = elevation;
-    this.name = name;
-  }
-
+  /**
+   * Creates a new Location object.
+   * @param account The account that this location is associated with.
+   * @param longitude The longitude of the location.
+   * @param latitude The latitude of the location.
+   * @param elevation The elevation of the location.
+   * @param name The name of the location.
+   */
   public Location(Account account,Double longitude, Double latitude,Double elevation, String name) {
     this.account = account;
     this.longitude = longitude;
