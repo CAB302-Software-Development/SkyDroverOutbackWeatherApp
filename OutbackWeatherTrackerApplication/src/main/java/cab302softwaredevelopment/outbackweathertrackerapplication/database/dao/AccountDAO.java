b01f840cc38a814d5e3e8c8055d8e4004601509d
joshua ward <joshuaward1000@gmail.com>
2024-09-22 01:04:42 +1000

    Added documentation for the AccountDAO class

--- OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/database/dao/AccountDAO.java
+++ OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/database/dao/AccountDAO.java
@@ -8,11 +8,24 @@ import jakarta.persistence.criteria.Root;
 import java.util.List;
 import org.hibernate.Session;
 
+/**
+ * A Data Access Object for the Account entity.
+ */
 public class AccountDAO {
 
   public AccountDAO() {
   }
 
+  /**
+   * Inserts a new Account into the database.
+   *
+   * @param account the Account object to be inserted
+   *                <p>
+   *                This method begins a transaction and attempts to save the provided Account
+   *                object to the database. If the operation is successful, the transaction is
+   *                committed. If an exception occurs during the operation, the transaction is
+   *                rolled back and the exception stack trace is printed.
+   */
   public void insert(Account account) {
     Session session = DatabaseConnection.getSession();
     try {
@@ -27,6 +40,16 @@ public class AccountDAO {
     }
   }
 
+  /**
+   * Updates an existing Account in the database.
+   *
+   * @param account the Account object to be updated
+   *                <p>
+   *                This method begins a transaction and attempts to update the provided Account
+   *                object in the database. If the operation is successful, the transaction is
+   *                committed. If an exception occurs during the operation, the transaction is
+   *                rolled back and the exception stack trace is printed.
+   */
   public void update(Account account) {
     Session session = DatabaseConnection.getSession();
     try {
@@ -41,6 +64,16 @@ public class AccountDAO {
     }
   }
 
+  /**
+   * Deletes an Account from the database by its ID.
+   *
+   * @param id The ID of the Account to be deleted
+   *           <p>
+   *           This method begins a transaction and attempts to delete the Account object with the
+   *           specified ID from the database. If the operation is successful, the transaction is
+   *           committed. If an exception occurs during the operation, the transaction is rolled
+   *           back and the exception stack trace is printed.
+   */
   public void delete(int id) {
     Session session = DatabaseConnection.getSession();
     try {
@@ -56,6 +89,16 @@ public class AccountDAO {
     }
   }
 
+  /**
+   * Deletes an Account from the database
+   *
+   * @param account The Account to be deleted
+   *                <p>
+   *                This method begins a transaction and attempts to delete the provided Account
+   *                object from the database. If the operation is successful, the transaction is
+   *                committed. If an exception occurs during the operation, the transaction is
+   *                rolled back and the exception stack trace is printed.
+   */
   public void delete(Account account) {
     Session session = DatabaseConnection.getSession();
     try {
@@ -70,6 +113,46 @@ public class AccountDAO {
     }
   }
 
+  /**
+   * Retrieves all Account objects from the database.
+   *
+   * @return A list of all Account objects in the database
+   */
+  @Deprecated
+  public List<Account> getAll() {
+    return new AccountQuery()
+        .getResults();
+  }
+
+  /**
+   * Retrieves an Account object from the database by its ID.
+   *
+   * @param id The ID of the Account to retrieve
+   * @return The Account object with the specified ID or null if no Account is found
+   */
+  @Deprecated
+  public Account getById(int id) {
+    return new AccountQuery()
+        .whereId(id)
+        .getSingleResult();
+  }
+
+  /**
+   * Retrieves an Account object from the database by its email.
+   *
+   * @param email The email of the Account to retrieve
+   * @return The Account object with the specified email or null if no Account is found
+   */
+  @Deprecated
+  public Account getByEmail(String email) {
+    return new AccountQuery()
+        .whereEmail(email)
+        .getSingleResult();
+  }
+
+  /**
+   * A query builder class for the Account entity.
+   */
   public static class AccountQuery {
     CriteriaQuery<Account> criteria;
     CriteriaBuilder builder;
@@ -83,26 +166,55 @@ public class AccountDAO {
       criteria.select(root);
     }
 
+    /**
+     * Adds an id filter to the query.
+     *
+     * @param id The ID to filter by
+     * @return This AccountQuery object
+     */
     public AccountQuery whereId(int id) {
       criteria.where(builder.equal(root.get("id"), id));
       return this;
     }
 
+    /**
+     * Adds an email filter to the query.
+     *
+     * @param email The email to filter by
+     * @return This AccountQuery object
+     */
     public AccountQuery whereEmail(String email) {
       criteria.where(builder.equal(root.get("email"), email));
       return this;
     }
 
+    /**
+     * Adds an email contains filter to the query.
+     *
+     * @param email The string to filter by
+     * @return This AccountQuery object
+     */
     public AccountQuery whereEmailLike(String email) {
       criteria.where(builder.like(root.get("email"), "%" + email + "%"));
       return this;
     }
 
+    /**
+     * Adds a Celsius preference filter to the query.
+     *
+     * @param preferCelsius The Celsius preference to filter for
+     * @return This AccountQuery object
+     */
     public AccountQuery wherePreferCelsius(boolean preferCelsius) {
       criteria.where(builder.equal(root.get("preferCelsius"), preferCelsius));
       return this;
     }
 
+    /**
+     * Executes the current filters and returns the results.
+     *
+     * @return A list of Account objects that match the provided filters
+     */
     public List<Account> getResults() {
       Session session = DatabaseConnection.getSession();
       List<Account> accounts = session.createQuery(criteria).getResultList();
@@ -110,32 +222,17 @@ public class AccountDAO {
       return accounts;
     }
 
+    /**
+     * Executes the current filters and returns the first result.
+     *
+     * @return The first Account object that matches the provided filters or null if no results are found
+     */
     public Account getSingleResult() {
       Session session = DatabaseConnection.getSession();
       Account account = session.createQuery(criteria).getResultStream().findFirst().orElse(null);
       session.close();
       return account;
     }
-  }
-
-  @Deprecated
-  public List<Account> getAll() {
-    return new AccountQuery()
-        .getResults();
-  }
-
-  @Deprecated
-  public Account getById(int id) {
-    return new AccountQuery()
-        .whereId(id)
-        .getSingleResult();
-  }
-
-  @Deprecated
-  public Account getByEmail(String email) {
-    return new AccountQuery()
-        .whereEmail(email)
-        .getSingleResult();
   }
 
 }
