1c4d2a1fd8cd7b7f9c6e488dcb17261f429e49f5
Dyllon <dyllontan1006@gmail.com>
2024-10-07 00:30:13 +1000

    weather-widget-3

---
+++ OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/controllers/widgets/WeatherWidget3Controller.java
@@ -0,0 +1,27 @@
+package cab302softwaredevelopment.outbackweathertrackerapplication.controllers.widgets;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class WeatherWidget3Controller {
+    
+    public ImageView weatherIconImageView;
+    public Text temperatureText;
+    public Text Location;
+    public Text Precipitation;
+    public Text Wind;
+    public Text Humidity;
+    public Text FeelsLike;
+
+    public void updateWeatherWidget(int temp, String location, String precipitation, int wind, int humid, int feels){
+        temperatureText.setText(temp + "°");
+        Location.setText(location);
+        Precipitation.setText(precipitation);
+        Wind.setText(wind + "km/h");
+        Humidity.setText(humid + "%");
+        FeelsLike.setText(feels + "°");
+
+
+    }
+
+}
