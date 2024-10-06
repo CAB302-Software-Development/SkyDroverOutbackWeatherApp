5d1c5bb25f1d33859b0cd2761412994e48c12297
Dyllon <dyllontan1006@gmail.com>
2024-10-06 17:08:12 +1000

    update-weather-widget2-controller

--- OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/controllers/widgets/WeatherWidget2Controller.java
+++ OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/controllers/widgets/WeatherWidget2Controller.java
@@ -1,17 +1,22 @@
 package cab302softwaredevelopment.outbackweathertrackerapplication.controllers.widgets;
 
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.image.Image;
 import javafx.scene.text.Text;
 
+import javax.sound.sampled.Line;
+
 public class WeatherWidget2Controller {
     public Text minMaxTempText;
-    public LineChart Graph;
+    public LineChart<Number,Number> Graph;
     public Text Location;
 
-    public void updateWeatherWidget(String location,int high, int low) {
+    public void updateWeatherWidget(String location, int high, int low, XYChart.Series<Number,Number> chart) {
         // Update weather condition
         Location.setText(location);
+
+        Graph.getData().add(chart);
 
         // Update high and low temperatures
         minMaxTempText.setText("H: " + high + "° L: " + low + "°");
