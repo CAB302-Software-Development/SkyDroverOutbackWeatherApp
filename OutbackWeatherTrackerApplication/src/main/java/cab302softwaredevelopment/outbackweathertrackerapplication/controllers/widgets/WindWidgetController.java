fb86a841edfc742092537cb3601396b440ff37f7
Jimbo-Wondall <109582514+Jimbo-Wondall@users.noreply.github.com>
2024-10-07 21:02:14 +1000

    Removed BaseWidgetController

--- OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/controllers/widgets/WindWidgetController.java
+++ OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/controllers/widgets/WindWidgetController.java
@@ -1,26 +1,51 @@
 package cab302softwaredevelopment.outbackweathertrackerapplication.controllers.widgets;
 
-import javafx.fxml.FXML;
+import cab302softwaredevelopment.outbackweathertrackerapplication.database.dao.HourlyForecastDAO;
+import cab302softwaredevelopment.outbackweathertrackerapplication.database.dao.LocationDAO;
+import cab302softwaredevelopment.outbackweathertrackerapplication.database.model.HourlyForecast;
+import cab302softwaredevelopment.outbackweathertrackerapplication.database.model.Location;
+import cab302softwaredevelopment.outbackweathertrackerapplication.models.DateData;
+import cab302softwaredevelopment.outbackweathertrackerapplication.utils.WidgetConfig;
 import javafx.scene.text.Text;
 import javafx.scene.control.Label;
-import java.awt.*;
 
-public class WindWidgetController extends BaseWidgetController {
+import java.time.ZonedDateTime;
+import java.util.Map;
+
+public class WindWidgetController implements IConfigurableWidget {
 
 
-    public Text Location;
+    public Text txtLocation;
     public Label Gust;
     public Label Wind;
     public Label Direction;
-    public Text Windspeed;
-    public Text GustSpeed;
+    public Text txtWindspeed;
+    public Text txtGustSpeed;
     public Text NSEW;
 
-    public void updateWindWidget(String location, int windspeed, int gustspeed, int dir){
-        Location.setText(location);
-        Windspeed.setText(windspeed+" km/h");
-        GustSpeed.setText(gustspeed+" km/h");
-        NSEW.setText(dir+"");
+    Location location = null;
+
+    public void applyConfig(WidgetConfig config) {
+        long locationId = config.getLocationId();
+
+        location = new LocationDAO.LocationQuery()
+                .whereId(locationId)
+                .getSingleResult();
+
+        updateWindWidget();
     }
 
+    public void updateWindWidget() {
+        int nowEpoch = (int) DateData.getNearestHourEpoch(ZonedDateTime.now());
+        HourlyForecast currentForecast = new HourlyForecastDAO.HourlyForecastQuery()
+                .whereTimestamp(nowEpoch)
+                .getSingleResult();
+
+        if (currentForecast != null) {
+            txtLocation.setText(currentForecast.getLocation().getName());
+            txtWindspeed.setText(currentForecast.getWind_direction_10m() + " km/h");
+            txtGustSpeed.setText(currentForecast.getWind_gusts_10m() + " km/h");
+            NSEW.setText(currentForecast.getWind_direction_10m() + "");
+        }
+    }
 }
