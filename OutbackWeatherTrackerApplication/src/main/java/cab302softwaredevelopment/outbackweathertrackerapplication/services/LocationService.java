c78485f7de71bd90307b135c3c584dc9c7a4b449
Jimbo-Wondall <109582514+Jimbo-Wondall@users.noreply.github.com>
2024-10-17 19:58:06 +1000

    Added endpoints to get coords from address, or vice versa

--- OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/services/LocationService.java
+++ OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/services/LocationService.java
@@ -4,23 +4,34 @@ import cab302softwaredevelopment.outbackweathertrackerapplication.database.dao.L
 import cab302softwaredevelopment.outbackweathertrackerapplication.database.model.Account;
 import cab302softwaredevelopment.outbackweathertrackerapplication.database.model.Location;
 import cab302softwaredevelopment.outbackweathertrackerapplication.models.LocationCreateModel;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import lombok.Getter;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 import java.util.List;
 
 public class LocationService {
-    private static LocationDAO locationDAO = new LocationDAO();
+    @Getter
+    private static LocationService instance = new LocationService();
 
-    public static void deleteLocation(long id) {
+    private LocationDAO locationDAO = new LocationDAO();
+
+    public void deleteLocation(long id) {
         locationDAO.delete(id);
     }
-    public static void deleteLocation(Location location) {
+    public void deleteLocation(Location location) {
         locationDAO.delete(location);
     }
 
-    public static void addLocationForCurrentUser(LocationCreateModel location) {
-        addLocationForUser(LoginState.getCurrentAccount(), location);
+    public void addLocationForCurrentUser(LocationCreateModel location) {
+        addLocationForUser(UserService.getInstance().getCurrentAccount(), location);
     }
-    public static void addLocationForUser(Account account, LocationCreateModel location) {
+    public void addLocationForUser(Account account, LocationCreateModel location) {
         Location newLocation = new Location(
                 account,
                 location.getLongitude(),
@@ -30,9 +41,81 @@ public class LocationService {
         locationDAO.insert(newLocation);
     }
 
-    public static List<Location> getCurrentUserLocations() {
+    public List<Location> getCurrentUserLocations() {
         return (new LocationDAO.LocationQuery())
-                .whereAccount(LoginState.getCurrentAccount())
+                .whereAccount(UserService.getInstance().getCurrentAccount())
                 .getResults();
     }
+
+    public String getAddressFromCoordinates(LocationCreateModel location) throws Exception {
+        return getAddressFromCoordinates(location.getLatitude(), location.getLongitude());
+    }
+    public String getAddressFromCoordinates(Location location) throws Exception {
+        return getAddressFromCoordinates(location.getLatitude(), location.getLongitude());
+    }
+    public String getAddressFromCoordinates(double lat, double lon) throws Exception {
+        String urlStr = String.format("https://nominatim.openstreetmap.org/reverse?format=json&lat=%f&lon=%f&addressdetails=1", lat, lon);
+
+        URL url = new URI(urlStr).toURL();
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("User-Agent", "Java Application");
+
+        int responseCode = conn.getResponseCode();
+        if (responseCode == 200) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // Use Gson to parse the JSON response
+            JsonObject jsonObject = JsonParser.parseString(response.toString()).getAsJsonObject();
+            String address = jsonObject.get("display_name").getAsString();
+
+            return address;
+        } else {
+            throw new Exception("HTTP Request failed with code: " + responseCode);
+        }
+    }
+
+    public LocationCreateModel geocodeAddress(String address) {
+        try {
+            String urlStr = String.format("https://nominatim.openstreetmap.org/search?q=%s&format=json&limit=1",
+                    address.replace(" ", "+"));
+
+            URL url = new URI(urlStr).toURL();
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("User-Agent", "Java Application");
+
+            int responseCode = conn.getResponseCode();
+            if (responseCode == 200) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                JsonObject jsonObject = JsonParser.parseString(response.toString()).getAsJsonArray().get(0).getAsJsonObject();
+                double latitude = jsonObject.get("lat").getAsDouble();
+                double longitude = jsonObject.get("lon").getAsDouble();
+
+                return new LocationCreateModel(null, latitude, longitude, 0);
+            } else {
+                throw new Exception("HTTP Request failed with code: " + responseCode);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
 }
