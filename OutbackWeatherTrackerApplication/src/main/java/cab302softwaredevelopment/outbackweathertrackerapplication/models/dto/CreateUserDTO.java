1a204739952788e5bdacf625f60a1fa628d885f7
SegusFaultise <wilsonzakk8@gmail.com>
2024-11-02 00:08:25 +1000

    Added new fields to some dto's and models, dont ask for more info find out for yourself or ill sudo kill <PID> my PID

--- OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/models/dto/CreateUserDTO.java
+++ OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/models/dto/CreateUserDTO.java
@@ -1,33 +1,54 @@
 package cab302softwaredevelopment.outbackweathertrackerapplication.models.dto;
 
+import com.google.gson.JsonObject;
 import lombok.Getter;
 import lombok.Setter;
 
 /**
- * This class represents the model used for creating a new user.
- * It contains the essential details such as username, password, and email required to create a user.
+ * This class represents the model used for creating a new user. It contains the essential details
+ * such as username, password, and email required to create a user.
  */
 @Setter
 @Getter
 public class CreateUserDTO {
 
-    /**
-     * The username of the new user.
-     */
-    private String username;
+  /**
+   * The username of the new user.
+   */
+  private String username;
 
-    /**
-     * The password for the new user account.
-     */
-    private String userPassword;
+  /**
+   * The password for the new user account.
+   */
+  private String userPassword;
 
-    /**
-     * The email address of the new user.
-     */
-    private String userEmail;
+  /**
+   * The email address of the new user.
+   */
+  private String userEmail;
 
-    /**
-     * The application theme of the new user.
-     */
-    private String userTheme;
+  /**
+   * The application theme of the new user.
+   */
+  private String userTheme;
+
+  /**
+   * The preferred Celsius of the user.
+   */
+  private boolean preferCelsius;
+
+  /**
+   * The selected layout of the user.
+   */
+  private String selectedLayout;
+
+  /**
+   * The dashboard layout of the user.
+   */
+  private JsonObject dashboardLayout;
+
+  /**
+   * The selected locations of the user.
+   */
+  private JsonObject locations;
 }
--- OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/models/dto/UpdateUserDTO.java
+++ OutbackWeatherTrackerApplication/src/main/java/cab302softwaredevelopment/outbackweathertrackerapplication/models/dto/UpdateUserDTO.java
@@ -1,28 +1,50 @@
 package cab302softwaredevelopment.outbackweathertrackerapplication.models.dto;
 
+import com.google.gson.JsonObject;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UpdateUserDTO {
-    /**
-     * The username of the updated user.
-     */
-    private String username;
 
-    /**
-     * The password for the new updated account.
-     */
-    private String userPassword;
+  /**
+   * The username of the updated user.
+   */
+  private String username;
 
-    /**
-     * The email address of the updated user.
-     */
-    private String userEmail;
+  /**
+   * The password for the new updated account.
+   */
+  private String userPassword;
 
-    /**
-     * The application theme of the updated user.
-     */
-    private String userTheme;
+  /**
+   * The email address of the updated user.
+   */
+  private String userEmail;
+
+  /**
+   * The application theme of the updated user.
+   */
+  private String userTheme;
+
+  /**
+   * The preferred Celsius of the user.
+   */
+  private boolean preferCelsius;
+
+  /**
+   * The selected layout of the user.
+   */
+  private String selectedLayout;
+
+  /**
+   * The dashboard layout of the user.
+   */
+  private JsonObject dashboardLayout;
+
+  /**
+   * The selected locations of the user.
+   */
+  private JsonObject locations;
 }
